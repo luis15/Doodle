42a6bf39e76d2e92c95eca9ba3408f916d55f7bf
luis15 <coelho.luisantonio@hotmail.com>
2017-02-10 22:13:09 -0200

    ver erro no server SMTP

--- servidor.js
+++ servidor.js
@@ -2,16 +2,27 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var request = require('request');
-
+var email = require('./lib/email');
 
 app.post('/enviar', function (req, res) {
   console.log("POST");
-       req.on('data', function (data) {
-           var body = JSON.parse(data);
-           console.log(body.assunto);
-           res.writeHead(200, {'Content-Type': 'text/html'});
-           res.end(JSON.stringify(body));
-       });
+   req.on('data', function (data) {
+       var body = JSON.parse(data);
+       email.enviarEmailContato(body.destinatarios, body.assunto, body.mensagem);
+       res.writeHead(200, {'Content-Type': 'text/html'});
+       res.end(JSON.stringify(body));
+       /*.then((body)=>{
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.end(JSON.stringify(body));
+       })
+       .catch((e) =>{
+         res.writeHead(500, {'Content-Type': 'text/html'});
+         res.end(e);
+       })*/
+   });
+})
+app.get('/', function (req, res) {
+  res.end("Olá curioso, o que faz por aqui?");
 })
 var server = app.listen(1000, function (req, res) {
 
